3c028513e884ee6fa5d2466ab5a3976f64623bb4
Miguel Cobas <cobas.miguel@gmail.com>
2018-08-09 18:20:26 -0500

    install react and react-dom

--- javascript/Curso_React_js/index.js
+++ javascript/Curso_React_js/index.js
@@ -1,1 +1,12 @@
-console.log('Hola Mundo!')
+import React from  'react';
+import ReactDom from 'react-dom';
+
+
+const container = document.getElementById('app');
+//ReactDom.render(que voy a renderizar, donde lo hare);
+
+const holaMundo = <h1>HOLA MUNDO </h1>;
+
+ReactDom.render(holaMundo, app);
+
+//console.log('Hola Mundo!')
