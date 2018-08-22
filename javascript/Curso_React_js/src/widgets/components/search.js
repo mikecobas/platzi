ebf8bf1ae0432e66ed88f67778875dd832ea7509
Miguel Cobas <cobas.miguel@gmail.com>
2018-08-21 20:39:03 -0500

    Se agrego Componente Search
    Se agrego el componente de Search

--- javascript/Curso_React_js/src/widgets/components/search.js
+++ javascript/Curso_React_js/src/widgets/components/search.js
@@ -7,12 +7,19 @@ import './search.css';
 //     )
 // }
 
-const Search = () => (
-    <form className="Search">
+const Search = (props) => (
+    <form 
+        className="Search"
+        onSubmit={props.handleSubmit}    
+    >
         <input 
+        ref={props.setRef}
         type="text" 
         placeholder="Busca tu video favorito"
         className="Search-input"
+        defaultValue='Platzi'
+        onChange={props.handleChange }
+        value={props.value}
         />     
     </form>
 )
--- javascript/Curso_React_js/src/widgets/containers/search.js
+++ javascript/Curso_React_js/src/widgets/containers/search.js
@@ -3,9 +3,29 @@ import Search from '../components/search'
 
 
 class SearchContainer extends Component {
+    state ={
+        value: 'platzi2'
+    }
+    handleSubmit = event =>{
+       event.preventDefault();
+       console.log( this.input.value, 'submit'); 
+    }
+    setInputRef =  element => {
+        this.input = element;
+    }
+    handleInputChange = event =>{
+        this.setState({
+            value:  event.target.value.replace(' ', '-')
+        })
+    }
     render(){
         return (
-            <Search />
+            <Search 
+            setRef={this.setInputRef}
+            handleSubmit={this.handleSubmit}
+            handleChange={this.handleInputChange}
+            value={this.state.value}
+            />
         )
     }
 }
